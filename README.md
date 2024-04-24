b6567a9010bd3adcaa196262ddb583cb80eac961
DBawazir2002 <boruto.laravel.naruto.23@gmail.com>
2024-04-24 19:49:47 +0300

    fix bug in connection.php

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # online-voting-system
 # It is an online voting website where the party can register itself on the website and then voters can vote for the party they want. This was build with native php using MVC architecture.
+# YOU MUST CHANGE THE USERNAME AND PASSWORD FOR DATABASE IN connection.php FILE
 # First, go to the SQL folder and make an import for this file from the phpMyAdmin panel
 # email: admin@gmail.com
 # password: admin
--- classes/connection.php
+++ classes/connection.php
@@ -4,9 +4,9 @@ class Dbh {
 
     protected function connect() {
         try {
-            $userName = "root";
-            $password = "";
-            $dbh = new PDO('mysql:host=localhost;dbname=onlineVotingSystem', $userName, $password);
+            $userName = "waziri";
+            $password = "0000";
+            $dbh = new PDO('mysql:host=localhost;dbname=onlinevotingsystem', $userName, $password);
             return $dbh;
         }
         catch (PDOException $th) {
